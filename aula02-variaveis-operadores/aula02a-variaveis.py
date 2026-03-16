a5d16b8bade5d283ec692cfd99cfea0e9aa5ccad
enzotabuchii <enzo.tabuchi78@gmail.com>
2026-03-16 20:45:51 -0300

    aula 2 variáveis

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,24 @@
+print("Olá, Mundo!")
+print(7 + 4)
+print("7" + "4")  # CONCATENANDO STRINGS
+
+# ISSO É UM COMENTÁRIO DE UMA LINHA
+"""
+    COMENTÁRIOS DE
+    MÚLTIPLAS LINHAS
+"""
+
+# VARIÁVEIS
+nome = "Enzo"
+idade = 18
+peso = 50.5
+
+print(nome, idade, peso)
+print(f"Olá, {nome}!")
+
+# INPUTS --- SIMULAÇÃO DE UM FORMS NO CMD
+nome = str(input("Qual é seu nome? "))
+idade = int(input("Qual a sua idade?"))
+peso = float(input("Qual é o seu peso? "))
+
+print(nome, idade, peso)
