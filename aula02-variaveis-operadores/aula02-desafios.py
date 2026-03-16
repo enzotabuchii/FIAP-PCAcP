fab121c9dfb086ed1c0a716257c39532d7ae25e9
enzotabuchii <enzo.tabuchi78@gmail.com>
2026-03-16 20:51:32 -0300

    Adding desafios

---
+++ aula02-variaveis-operadores/aula02-desafios.py
@@ -0,0 +1,17 @@
+# Desafio 1
+nome = str(input("Digite seu nome: "))
+
+print(f"Bem vindo(a), {nome}")
+
+# Desafio 2
+dia = int(input("Digite o dia do seu nascimento: "))
+mes = int(input("Digite o mês do seu nascimento: "))
+ano = int(input("Digite o ano do seu nascimento: "))
+
+print(f"Você nasceu em: {dia}/{mes}/{ano}")
+
+# Desafio 3
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+print(f"A soma dos número é de {num1 + num2}")
