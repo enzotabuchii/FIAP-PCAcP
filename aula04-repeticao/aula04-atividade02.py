fa9b38c3befcef6a32835975c7f0f3a673b03a56
enzotabuchii <enzo.tabuchi78@gmail.com>
2026-04-27 20:53:37 -0300

    Aula 4 repetição

---
+++ aula04-repeticao/aula04-atividade02.py
@@ -0,0 +1,14 @@
+def verificar_nota (nota):
+    while nota < 0 or nota > 10:
+        print("A nota deve estar entre 0 e 10")
+        nota = float(input("Digite a nota novamente: "))
+    return nota
+
+notaA = float(input("Digite a 1º nota: "))
+notaA = verificar_nota(notaA)
+
+notaB = float(input("Digite a 2º nota: "))
+notaB = verificar_nota(notaB)
+
+media = (notaA + notaB) / 2
+print(media)
